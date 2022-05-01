c1424c6ddceaed67b15b6e195d2d92f76def9096
jduo <jduo@juju.com>
2022-05-01 03:01:08 -0300

    Use new HTTP api

--- main.py
+++ main.py
@@ -13,96 +13,87 @@ def stream_web_log():
         msg = l[2].format(*l[3], **l[4])
         yield '{}:{}: {}\n'.format(l[0], log.INT_TO_LABEL[l[1]], msg)
 
-
 def stream_web_log_frequency():
     for k in sorted(log.web_log_frequency):
         v = log.web_log_frequency[k]
         yield '{}:{}: {}: {}\n'.format(v['last_seen'], log.INT_TO_LABEL[v['level']], k, v['count'])
 
+wifi_tracker = solar.WifiTracker(config.AP_WIFI_ESSID, config.AP_WIFI_PASSWORD)
+solar_manager = solar.SolarManager(wifi_tracker)
+app = webserver.Server(static_path='/static/',
+                       auth_token=config.AUTH_TOKEN,
+                       pre_request_hook=lambda: uasyncio.create_task(wifi_tracker.blink()))
 
-async def serve_request(verb, path, request_trailer):
-    log.debug(path)
-    uasyncio.create_task(wifi_tracker.blink())
-    content_type = 'application/json'
-    status = 200
-    if path == '/devicesread':
-        payload = ujson.dumps(solar_manager.latest_read())
-    elif path == '/history':
-        if verb == webserver.POST:
-            cfg = webserver.extract_json(request_trailer)
-            solar_manager.set_json(cfg)
-        payload = ujson.dumps(solar_manager.get_json())
-    elif path == '/resistance':
-        value = solar_manager.get_resistance()
-        if verb == webserver.POST:
-            value = solar_manager.set_resistance(not value)
-        payload = ujson.dumps(dict(value=value))
-    elif path == '/reset':
-        if verb == webserver.POST:
-            solar_manager.reset()
-            log.web_log_history.clear()
-            log.web_log_frequency.clear()
-            log.important('Resetting server status...')
-        payload = ujson.dumps('')
-    elif path == '/logcfg':
-        if verb == webserver.POST:
-            cfg = webserver.extract_json(request_trailer)
-            log.LOG_LEVEL = cfg.get('log_level', log.LOG_LEVEL)
-            log.WEB_LOG_LEVEL = cfg.get('web_log_level', log.WEB_LOG_LEVEL)
-            log.WEB_LOG_SIZE = cfg.get('web_log_size', log.WEB_LOG_SIZE)
-        payload = ujson.dumps(dict(log_level=log.LOG_LEVEL,
-                                   web_log_level=log.WEB_LOG_LEVEL,
-                                   web_log_size=log.WEB_LOG_SIZE))
-    elif path == '/log':
-        if verb == webserver.POST:
-            log.web_log_history.clear()
-            log.web_log_frequency.clear()
-        payload = ujson.dumps(dict(log=log.web_log_history, frequency=log.web_log_frequency))
-    elif path == '/logs':
-        content_type = 'text/plain'
-        payload = stream_web_log()
-    elif path == '/logfrequency':
-        content_type = 'text/plain'
-        payload = stream_web_log_frequency()
-    elif path == '/wifioff':
-        v = False
-        if verb == webserver.POST:
-            wifi_tracker.schedule_toggle = True
-            v = True
-        payload = ujson.dumps(dict(wifioff=v))
-    else:
-        content_type = 'text/html'
-        if path == '/':
-            resp = webserver.response(status, content_type, '')
-            return resp, webserver.serve_file('/client.html', {'@=SERVER_ADDRESS=@':''})
-        else:
-            status = 404
-            payload = webserver.web_page('404 Not found')
-    return webserver.response(status, content_type, payload)
+@app.json()
+def devicesread(verb, _):
+    return solar_manager.latest_read()
 
+@app.json()
+def history(verb, cfg):
+    if verb == webserver.POST:
+        solar_manager.set_json(cfg)
+    return solar_manager.get_json()
 
-solar_manager = None
-wifi_tracker = None
+@app.json()
+def resistance(verb, _):
+    value = solar_manager.get_resistance()
+    if verb == webserver.POST:
+        value = solar_manager.set_resistance(not value)
+    return dict(value=value)
+
+@app.json()
+def reset(verb, _):
+    if verb == webserver.POST:
+        solar_manager.reset()
+        log.web_log_history.clear()
+        log.web_log_frequency.clear()
+        log.important('Resetting server status...')
+    return ''
+
+@app.json()
+def logcfg(verb, cfg):
+    if verb == webserver.POST:
+        log.LOG_LEVEL = cfg.get('log_level', log.LOG_LEVEL)
+        log.WEB_LOG_LEVEL = cfg.get('web_log_level', log.WEB_LOG_LEVEL)
+        log.WEB_LOG_SIZE = cfg.get('web_log_size', log.WEB_LOG_SIZE)
+    return dict(log_level=log.LOG_LEVEL,
+                web_log_level=log.WEB_LOG_LEVEL,
+                web_log_size=log.WEB_LOG_SIZE)
+
+@app.json()
+def wifioff(verb, _):
+    v = False
+    if verb == webserver.POST:
+        wifi_tracker.schedule_toggle = True
+        v = True
+    return dict(wifioff=v)
+
+@app.plain()
+def logs(verb, _):
+    return stream_web_log()
+
+@app.plain()
+def logfrequency(verb, _):
+    return stream_web_log_frequency()
+
+@app.html('/')
+def index(verb, _):
+    return webserver.serve_file('/client.html', {'@=SERVER_ADDRESS=@':'',
+                                                 '@=AUTH_TOKEN=@':config.AUTH_TOKEN})
+
 def main():
-    global solar_manager, wifi_tracker
-    wifi_tracker = solar.WifiTracker(config.AP_WIFI_ESSID, config.AP_WIFI_PASSWORD)
-    solar_manager = solar.SolarManager(wifi_tracker)
     gmt, localt = utime.gmtime(), utime.localtime()
     assert gmt == localt
-    server = webserver.Server(serve_request)
-    server.static_path = '/static/'
     log.garbage_collect()
     log.LOG_LEVEL = log.INFO
     try:
         wifi_tracker.on()
-        uasyncio.run(server.run())
+        uasyncio.run(app.run())
         uasyncio.run(solar_manager.loop_tasks())
     finally:
-        uasyncio.run(server.close())
+        uasyncio.run(app.close())
         _ = uasyncio.new_event_loop()
     wifi_tracker.off()
-
-
 
 if __name__ == '__main__':
     main()
--- webserver.py
+++ webserver.py
@@ -5,7 +5,6 @@ import sys
 import log
 
 
-AUTH_TOKEN='1234'
 CONN_TIMEOUT=10
 STATUS_CODES = {
     200:'OK',
@@ -54,13 +53,60 @@ class StopWebServer(Exception):
     pass
 
 
-def extract_json(request):
+def extract_json(payload, auth_token):
     log.garbage_collect()
-    msg = ujson.loads(request[request.rfind(b'\r\n\r\n')+4:])
-    if msg.get('auth_token') != AUTH_TOKEN:
+    msg = ujson.loads(payload[payload.rfind(b'\r\n\r\n')+4:])
+    if msg.get('auth_token') != auth_token:
         raise UnauthorizedError('Unauthorized. Send {"auth_token":"<secret>", "payload": ...}')
     return msg['payload']
 
+
+def jsondumps(o, depth=1):
+    # Memory efficient Json String generator
+    if depth and isinstance(o, dict):
+        for s in _jsondumps_dict(o, depth):
+            yield s
+    elif depth and isinstance(o, (list, tuple, set)):
+        for s in _jsondumps_iter(o, depth):
+            yield s
+    else:
+        yield ujson.dumps(o)
+
+
+def _jsondumps_iter(o, depth=1):
+    depth -= 1
+    yield '['
+    count = 0
+    lgth = len(o)
+    for v in o:
+        if depth:
+            for s in jsondumps(v, depth):
+                yield s
+        else:
+            yield ujson.dumps(v)
+        count += 1
+        if lgth != count:
+            yield ' ,'
+    yield ']'
+
+
+def _jsondumps_dict(d, depth=1):
+    depth -= 1
+    yield '{'
+    count = 0
+    lgth = len(d)
+    for k,v in d.items():
+        yield ujson.dumps(k)
+        yield ' : '
+        if depth:
+            for s in jsondumps(v, depth):
+                yield s
+        else:
+            yield ujson.dumps(v)
+        count += 1
+        if lgth != count:
+            yield ' ,'
+    yield '}'
 
 def serve_file(path, replacements=None):
     if replacements:
@@ -88,19 +134,124 @@ def yield_lines(path, replacements):
             size += len(chunk)
             if size // CHUNK_SIZE:
                 log.garbage_collect()
-                size = size % CHUNK_SIZE
+                size = 0
             chunk = fp.readline()
 
 
+def urldecode_plus(s):
+    s = s.replace('+', ' ')
+    arr = s.split('%')
+    res = arr[0]
+    for it in arr[1:]:
+        if len(it) >= 2:
+            res += chr(int(it[:2], 16)) + it[2:]
+        elif len(it) == 0:
+            res += '%'
+        else:
+            res += it
+    return res
+
+
+def parse_query_string(s):
+    s = s.strip()
+    res = {}
+    if not s:
+        return res
+    pairs = s.split('&')
+    for p in pairs:
+        vals = [urldecode_plus(x) for x in p.split('=', 1)]
+        if len(vals) == 1:
+            res[vals[0]] = ''
+        else:
+            res[vals[0]] = vals[1]
+    return res
+
+
+def file_exists(path):
+    try:
+        with open(path):
+            pass
+        return True
+    except:
+        return False
+
+
+_endpoints = {}
 class Server:
+
+    class _endpoint_decorator:
+        # Base class to later do @app.json() or @app.html() decorations
+        def __init__(self, path=None,
+                           response_builder=None,
+                           extra_headers=EXTRA_HEADERS,
+                           stream=False,
+                           is_async=False,
+                           **options):
+            self.path = path
+            self.options = dict(extra_headers=extra_headers,
+                                response_builder=response_builder,
+                                stream=stream,
+                                is_async=is_async,
+                                endpoint_type=self.__class__,
+                                content_type=self.content_type,
+                                **options
+                                )
+        def __call__(self, method):
+            path = self.path or '/' + method.__name__
+            _endpoints[path] = dict(method=method, options=self.options)
+            return method
+    class json(_endpoint_decorator):
+        content_type = 'application/json'
+        def __init__(self, response_builder=None,
+                           extra_headers=EXTRA_HEADERS,
+                           stream=False,
+                           is_async=False,
+                           auto_json=True,
+                           auto_json_depth=0):
+            super().__init__(response_builder=response_builder,
+                             extra_headers=extra_headers,
+                             stream=stream,
+                             is_async=is_async,
+                             auto_json=auto_json,
+                             auto_json_depth=auto_json_depth,
+                             )
+    class html(_endpoint_decorator):
+        content_type = 'text/html'
+    class plain(_endpoint_decorator):
+        content_type = 'text/plain'
+
+    def _default_method(self, v,req,**params):
+        return 'Not Found\n{}\n{}\n{}'.format(v, req, params)
+
     static_path = None
     static_files_replacements = {}
-    def __init__(self, serve_request, host='0.0.0.0', port=80, backlog=5, timeout=CONN_TIMEOUT):
-        self.serve_request = serve_request
+
+    def __init__(self,
+                 host='0.0.0.0',
+                 port=80,
+                 backlog=5,
+                 timeout=CONN_TIMEOUT,
+                 auth_token='',
+                 static_path=None,
+                 static_files_replacements=None,
+                 pre_request_hook=None
+                 ):
         self.host = host
         self.port = port
         self.backlog = backlog
         self.timeout = timeout
+        self.auth_token = auth_token
+        self.static_path = static_path
+        self.static_files_replacements = static_files_replacements
+        self.pre_request_hook = pre_request_hook
+        self.default_endpoint = dict(method=self._default_method,
+                                     options=dict(
+                                         endpoint_type='default',
+                                         content_type='text/plain',
+                                         extra_headers=EXTRA_HEADERS,
+                                         response_builder=response,
+                                         status=404,
+                                     ))
     async def run(self):
         log.debug('Opening address={host} port={port}.', host=self.host, port=self.port)
         self.conn_id = 0 #connections ids
@@ -111,15 +262,15 @@ class Server:
         log.debug('Accepting conn_id={conn_id}', conn_id=conn_id)
         log.garbage_collect()
         try:
-            request = await uasyncio.wait_for(sreader.readline(), self.timeout)
-            request_trailer = await uasyncio.wait_for(sreader.read(-1), self.timeout)
-            log.debug('request={request!r}, conn_id={conn_id}', request=request, conn_id=conn_id)
-            verb, path = request.decode('utf8').split()[0:2]
+            verb, path, query_string = await self.read_request_line(sreader, self.timeout)
+            payload = await uasyncio.wait_for(sreader.read(-1), self.timeout)
+            log.debug('request={request!r}, conn_id={conn_id}', request=path, conn_id=conn_id)
             try:
                 if self.static_path and path.startswith(self.static_path) and verb == GET:
                     resp = self.serve_static(path)
                 else:
-                    resp = await self.serve_request(verb, path, request_trailer)
+                    params = parse_query_string(query_string)
+                    resp = await self.serve_request(verb, path, params, payload, swriter)
             except UnauthorizedError as e:
                 resp = response(401, 'text/html', web_page('{} {!r}'.format(e,e)))
             await self.send_response(swriter, resp)
@@ -143,51 +294,79 @@ class Server:
         self.server.close()
         await self.server.wait_closed()
         log.info('Server closed.')
+    def serve_static(self, path):
+        if file_exists(path):
+            content_type = 'text/html'
+            if path.endswith('.js'):
+                content_type = 'application/javascript'
+            return response(200, content_type, serve_file(path, self.static_files_replacements))
+        return response(404, 'text/html', web_page('404 Not Found'))
+    async def read_request_line(self, sreader, timeout=CONN_TIMEOUT):
+        while True:
+            rl = await uasyncio.wait_for(sreader.readline(), timeout)
+            # skip empty lines
+            if rl == b'\r\n' or rl == b'\n':
+                continue
+            break
+        rl_frags = rl.decode('utf8').split()
+        if len(rl_frags) != 3:
+            raise LookupError()
+        verb = rl_frags[0]
+        url_frags = rl_frags[1].split('?', 1)
+        path = url_frags[0]
+        query_string = ''
+        if len(url_frags) > 1:
+            query_string = url_frags[1]
+        return verb, path, query_string
+    async def serve_request(self, verb, path, params, req_payload, swriter):
+        if self.pre_request_hook:
+            self.pre_request_hook()
+        endpoint = _endpoints.get(path, self.default_endpoint)
+        options = endpoint['options']
+        if verb == POST and options.get('auto_json'):
+            req_payload = self.json_load(req_payload)
+        if options.get('stream'):
+            resp_payload = endpoint['method'](verb, req_payload, swriter, **params)
+        else:
+            resp_payload = endpoint['method'](verb, req_payload, **params)
+        if options.get('is_async'):
+            resp_payload = await resp_payload
+        if options.get('auto_json'):
+            resp_payload = self.json_dump(resp_payload, options.get('auto_json_depth', 0))
+        response_builder = options['response_builder'] or response
+        return response_builder(options.get('status', 200), options['content_type'], resp_payload, extra_headers=options['extra_headers'])
     async def send_response(self, swriter, resp):
         if isinstance(resp, (str, bytes)):
             if resp:
                 swriter.write(resp)
                 return len(resp)
             return 0
+        #elif iscoroutine(resp): # Disabled. We can't distiguish a generator from a coroutine
+        #    return await self.send_response(swriter, await resp)
         else:
             count = 0
             for l in resp:
                 count += await self.send_response(swriter, l)
                 if count // CHUNK_SIZE:
                     await swriter.drain()
-                    count = count % CHUNK_SIZE
+                    count = 0
             return count
-    def serve_static(self, path):
-        if self.file_exists(path):
-            content_type = 'text/html'
-            if path.endswith('.js'):
-                content_type = 'application/javascript'
-            return response(200, content_type, serve_file(path, self.static_files_replacements))
-        return response(404, 'text/html', web_page('404 Not Found'))
-    def file_exists(self, path):
-        try:
-            with open(path):
-                pass
-            return True
-        except:
-            return False
+    def json_load(self, payload):
+        return extract_json(payload, self.auth_token)
+    def json_dump(self, obj, depth=1):
+        return jsondumps(obj, depth)
+
 
 
 def main():
-    async def serve_request(verb, path, request_trailer):
-        if path == '/redirect':
-            return redirect('http://192.168.2.107/static/client.html')
-        else:
-            return response(200, 'text/html', web_page(
-                '<h2>Hellow World</h2><pre>{}</pre>'.format(request_trailer.decode('utf8'))))
-    server = Server(serve_request)
-    server.static_path = '/static/'
+    app = Server(static_path='/static/')
+    log.LOG_LEVEL = log.DEBUG
     log.garbage_collect()
     try:
-        uasyncio.run(server.run())
+        uasyncio.run(app.run())
         uasyncio.get_event_loop().run_forever()
     finally:
-        uasyncio.run(server.close())
+        uasyncio.run(app.close())
         _ = uasyncio.new_event_loop()
 
 
